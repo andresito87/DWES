597b9c702709f6109f9f7da0a5b8012c1c5a2c9c
Andres <andrespodadera87@gmail.com>
2024-05-01 18:36:49 +0200

    completando ejercicio 1

--- dwes07/Podadera_Gonzalez_Andres_Samuel_DWES07/parte1/etc/config.php
+++ dwes07/Podadera_Gonzalez_Andres_Samuel_DWES07/parte1/etc/config.php
@@ -1,5 +1,5 @@
 <?php
-
-define ("VIRUS_TOTAL_API_KEY","1234123412341234"); //Rellena aquí tu propia API KEY
-define ("DATOS",__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'subidas'.DIRECTORY_SEPARATOR.'datos_archivos_subidos.ser');
-define ("CARPETA_SUBIDAS",__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'subidas');
+// TODO: Dejar el API KEY vacío para entregar la práctica
+define("VIRUS_TOTAL_API_KEY", "5571bf5b1de2a3d52f40d9d20b2fe76a0e47f320968049947011056e0ef8fcdd");
+define("DATOS", __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'subidas' . DIRECTORY_SEPARATOR . 'datos_archivos_subidos.ser');
+define("CARPETA_SUBIDAS", __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'subidas');
